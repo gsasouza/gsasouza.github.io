1273d61251c52e0e43d9074f3c6a06c566026a66
Gabriel Souza <gabrie.ssouza@gmail.com>
2019-03-12 22:58:30 -0300

    chore(styles): improve styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gabriel Souza`,
+    description: `Full Stack JavaScript developer working on bleeding edge`,
+    author: `@gsasouza`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-typescript',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
